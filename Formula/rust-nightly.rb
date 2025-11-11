307abbaf46ba24fe110ae0439273ff4639ebfb04
Heath Stewart <heaths@outlook.com>
2025-11-11 02:04:38 -0800

    Fix rust-nightly formula

--- Formula/rust-nightly.rb
+++ Formula/rust-nightly.rb
@@ -1,31 +1,31 @@
 class RustNightly < Formula
   desc "Install Rust nightly toolchain"
   homepage "https://www.rust-lang.org"
-  version "nightly-2025-11-11"
+  version "2025-11-11"
   license "Apache-2.0"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://static.rust-lang.org/dist/#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "8c3426d9a06f20016f68636478b8f3f2077c92abb73f947bd9ecc7a7da084a5d"
+      url "https://static.rust-lang.org/dist/#{version}/rust-nightly-aarch64-apple-darwin.tar.gz"
+      sha256 "faca660ec8122e515cbd3e3032ef4c2f152e0ef4cf85ff3f786d70d1dd6cff2e"
     else
-      url "https://static.rust-lang.org/dist/#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "f9181ead1af1a2c18124e9cf3a198032b4e9d138783be9433fc4b514712a84b3"
+      url "https://static.rust-lang.org/dist/#{version}/rust-nightly-x86_64-apple-darwin.tar.gz"
+      sha256 "e5baf7f775760ff3f6056f17cc4d3e84592634e86de0fb3cf333d2790c6d068f"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://static.rust-lang.org/dist/#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9c7f14b3dee3fc57125847f7dfc6fe8064c0081bc2d666a32519859672ffbff7"
+      url "https://static.rust-lang.org/dist/#{version}/rust-nightly-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "f6193fea528778ac6bbe0e503aa235d58ef907535a7d9b0f27a642c08576dde8"
     else
-      url "https://static.rust-lang.org/dist/#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "4856cccb8f7d566b881f354743ac39dd705536f2da2623c4df6ec14524d8ada2"
+      url "https://static.rust-lang.org/dist/#{version}/rust-nightly-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "6e18cd9290b004c372e8497e1d118ecccf670d6c0bd527f67942e8605582cfdd"
     end
   else
     odie "Unsupported platform for Rust nightly"
   end
 
   def install
-    system "./install.sh", "--prefix=#{prefix}", "--disable-ldconfig"
+    system "./install.sh", "--prefix=#{prefix}"
   end
 
   test do
