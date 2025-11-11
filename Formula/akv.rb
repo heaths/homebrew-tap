5c033a208249ba4b1daca9470684faf321a95339
Heath Stewart <heaths@outlook.com>
2025-11-11 02:43:16 -0800

    Make sure bin exists

--- Formula/akv.rb
+++ Formula/akv.rb
@@ -21,6 +21,8 @@ class Akv < Formula
     system "rustup", "default", "nightly"
     ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
     system "cargo", "install", *std_cargo_args
+    shell_output("ls -l #{bin}")
+    shell_output("#{bin}/akv --version")
     generate_completions_from_executable(bin/"akv", "completion")
   end
 
