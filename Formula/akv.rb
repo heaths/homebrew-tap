e87cd1adb374ca6312ce22196189f0ccb6e80cc8
Heath Stewart <heaths@outlook.com>
2025-11-12 01:27:12 +0000

    akv: update 0.7.2 bottle.

--- Formula/akv.rb
+++ Formula/akv.rb
@@ -8,10 +8,10 @@ class Akv < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/heaths/tap"
-    sha256 cellar: :any,                 arm64_sequoia: "a326dddc39e0b9a5ec45d44f9c1856f1e5c1d3e7301fc33e0611af877b3b10fb"
-    sha256 cellar: :any,                 arm64_sonoma:  "e0eb13e2b6c1b2ca0a567aaa487184e1140c761e7b1d3e2b1675beb96a507fa9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9e46f1a7db28105c88aeeb79cdc0dae52b477a68268a0f8685f769decac9e7bc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c0b4ca990820b3e4f7bc52809203b2d9c1ad1e25204cec4f114a44c73a348dc"
+    sha256 cellar: :any,                 arm64_sequoia: "fb0b83800dacce48a17ee712be6e5c38b792602f62717ed690b1a8f34ca4245b"
+    sha256 cellar: :any,                 arm64_sonoma:  "7b92c2fdd055f8f71c80f7043dbe3b34eb89c4bd8acf8f22e526d40a86f07b44"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "662eb2b3f72fd60200b962a7cbb59dd828979e94596ba1746b2b600962fc6696"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4cf1d8a352cb3a3d5b8ff3024d6ba8d2b89b16b90241d0e0ee16d377bdd7d4b0"
   end
 
   depends_on "rustup" => :build
