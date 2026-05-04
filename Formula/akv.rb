2657fcda7ee76b12363d259f635ddfe08742b9bf
Heath Stewart <heaths@outlook.com>
2026-05-04 19:56:25 +0000

    akv 0.10.2
    Closes #23.
    
    Signed-off-by: Heath Stewart <heaths@outlook.com>

--- Formula/akv.rb
+++ Formula/akv.rb
@@ -1,8 +1,8 @@
 class Akv < Formula
   desc "Azure Key Vault CLI"
   homepage "https://github.com/heaths/akv-cli-rs"
-  url "https://github.com/heaths/akv-cli-rs/archive/refs/tags/v0.10.1.tar.gz"
-  sha256 "21e0fbc8958debf22189a167b7e17a294a96cbe0e98613c40da27b3f7378c0c9"
+  url "https://github.com/heaths/akv-cli-rs/archive/refs/tags/v0.10.2.tar.gz"
+  sha256 "fa6d82c4ec64f2e6f607a88a3e3133262156473f8d4ae65e7d5805188b2039ce"
   license "MIT"
   head "https://github.com/heaths/akv-cli-rs.git", branch: "main"
 
