5680176acc2703263c148c1a000c426500abf5c8
Heath Stewart <heaths@outlook.com>
2026-06-10 06:19:10 +0000

    akv 0.11.1

--- Formula/akv.rb
+++ Formula/akv.rb
@@ -1,8 +1,8 @@
 class Akv < Formula
   desc "Azure Key Vault CLI"
   homepage "https://github.com/heaths/akv-cli-rs"
-  url "https://github.com/heaths/akv-cli-rs/archive/refs/tags/v0.11.0.tar.gz"
-  sha256 "aa90f6d96d01b44f92d7d4c2a1a504196c9f46dabd58d037c65f5493a40a735c"
+  url "https://github.com/heaths/akv-cli-rs/archive/refs/tags/v0.11.1.tar.gz"
+  sha256 "420ffaef6fdf0359fb5308111a1698fa5654cba05c91b1714caac21bfbfb037f"
   license "MIT"
   head "https://github.com/heaths/akv-cli-rs.git", branch: "main"
 
